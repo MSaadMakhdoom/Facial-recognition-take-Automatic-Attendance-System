2cdc7c2d4bd6230bff9d5152b6ad1a99bf345fe3
MSaadMakhdoom <62068377+MSaadMakhdoom@users.noreply.github.com>
2023-06-18 20:43:10 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -134,8 +134,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = "makhdoomrasheed991@gmail.com"
-EMAIL_HOST_PASSWORD = "fumrszzkkzefhkjd"
+EMAIL_HOST_USER = "setyour@gmail.com"
+EMAIL_HOST_PASSWORD = "set password"
 
 
 EMAIL_USE_TLS = True
